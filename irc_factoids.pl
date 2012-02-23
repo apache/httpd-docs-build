75087a88a7b4deb85d0eebf2ff282acbe1b57b8e
Richard Bowen <rbowen@apache.org>
2012-02-23 20:10:19 +0000

    Reverts part of previous change - don't need the nick to send the message in /msg
    git-svn-id: https://svn.apache.org/repos/asf/httpd/docs-build/trunk@1292934 13f79535-47bb-0310-9956-ffa450edef68

--- irc_factoids.pl
+++ irc_factoids.pl
@@ -4,10 +4,10 @@ use Getopt::Std;
 use Data::Dumper;
 use strict;
 
-our ( $opt_m, $opt_v, $opt_b );
+our ( $opt_m, $opt_v );
 our $VERSION = '0.01';
 
-getopts("m:v:b:");
+getopts("m:v:");
 HELP_MESSAGE('The -m option is required.') unless $opt_m;
 
 $opt_m =~ s/\.xml$//;
@@ -15,14 +15,11 @@ HELP_MESSAGE('File '. $opt_m . '.xml not found.') unless -f $opt_m . '.xml';
 
 my $file = $opt_m . '.xml';
 
-my $bot = $opt_b ? $opt_b : 'fajita';
-my $addr = $bot . ': ';
-
 my $xs = XML::Simple->new();
 my $xml = $xs->XMLin( $file );
 
-print $addr . "forget $opt_m\n";
-print $addr . "$opt_m is " . 'http://httpd.apache.org/docs/' . ( $opt_v ? $opt_v : 'current' ) . '/mod/' . $opt_m .  '.html' . "\n\n";
+print "forget $opt_m\n";
+print "$opt_m is " . 'http://httpd.apache.org/docs/' . ( $opt_v ? $opt_v : 'current' ) . '/mod/' . $opt_m .  '.html' . "\n\n";
 
 my @directives;
 foreach my $directive ( sort( keys %{ $xml->{directivesynopsis} } ) ) {
@@ -32,28 +29,29 @@ foreach my $directive ( sort( keys %{ $xml->{directivesynopsis} } ) ) {
     my $desc = $d->{description}; $desc =~ s/[\r\n]/ /gs;
     my $name = $directive . ( $opt_v ? " $opt_v" : '' );
 
-    print $addr . "forget $name\n";
-    print $addr . "$name is ";
+    print "forget $name\n";
+    print "$name is ";
     print 'http://httpd.apache.org/docs/' . ( $opt_v ? $opt_v : 'current' ) . '/mod/' . $opt_m .  '.html#' . lc( $directive ) .  " - ";
     print $desc . "\n";
 
-    print $addr . "forget $name default\n";
+    print "forget $name default\n";
     if ( $d->{default} ) {
-        print $addr . "$name default is " . $d->{default} . "\n";
+        print "$name default is " . $d->{default} . "\n";
     } else {
-        print $addr . "$name default is <reply>$name has not default value";
+        print "$name default is <reply>$name has no default value\n";
     }
 
-    print $addr . "forget $name override\n";
+    print "forget $name override\n";
     if ( $d->{override} ) {
-        print $addr . "$name override is <reply>$name may be used in an .htaccess file if AllowOverride is set to ".$d->{override}."\n";
+        print "$name override is <reply>$name may be used in an .htaccess file if AllowOverride is set to ".$d->{override}."\n";
     } else {
-        print $addr . "$name override is <reply>$name may not be used in a .htaccess file\n";
+        print "$name override is <reply>$name may not be used in a .htaccess file\n";
     }
 
-    print $addr . "forget $name context\n";
-    print $addr . "$name context is <reply>$name may be used in the following contexts: ";
-    print ( join ', ', @{ $d->{contextlist}->{context} } ) . "\n";
+    print "forget $name context\n";
+    print "$name context is <reply>$name may be used in the following contexts: ";
+    my $contexts = ref( $d->{contextlist}->{context} ) ?  $d->{contextlist}->{context} : [ $d->{contextlist}->{context} ];
+    print ( join ', ', @{ $contexts } ) . "\n";
     print "\n\n";
 
 }
