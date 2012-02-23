db9d00c40f75f665216b89dfc7e074c109f7cf8e
Richard Bowen <rbowen@apache.org>
2012-02-23 20:17:16 +0000

    Makes the 'forget ...' statements optional, triggered with the -f flag.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/docs-build/trunk@1292936 13f79535-47bb-0310-9956-ffa450edef68

--- irc_factoids.pl
+++ irc_factoids.pl
@@ -4,10 +4,10 @@ use Getopt::Std;
 use Data::Dumper;
 use strict;
 
-our ( $opt_m, $opt_v );
+our ( $opt_m, $opt_v, $opt_f );
 our $VERSION = '0.01';
 
-getopts("m:v:");
+getopts("fm:v:");
 HELP_MESSAGE('The -m option is required.') unless $opt_m;
 
 $opt_m =~ s/\.xml$//;
@@ -18,7 +18,7 @@ my $file = $opt_m . '.xml';
 my $xs = XML::Simple->new();
 my $xml = $xs->XMLin( $file );
 
-print "forget $opt_m\n";
+print "forget $opt_m\n" if $opt_f;
 print "$opt_m is " . 'http://httpd.apache.org/docs/' . ( $opt_v ? $opt_v : 'current' ) . '/mod/' . $opt_m .  '.html' . "\n\n";
 
 my @directives;
@@ -29,26 +29,26 @@ foreach my $directive ( sort( keys %{ $xml->{directivesynopsis} } ) ) {
     my $desc = $d->{description}; $desc =~ s/[\r\n]/ /gs;
     my $name = $directive . ( $opt_v ? " $opt_v" : '' );
 
-    print "forget $name\n";
+    print "forget $name\n" if $opt_f;
     print "$name is ";
     print 'http://httpd.apache.org/docs/' . ( $opt_v ? $opt_v : 'current' ) . '/mod/' . $opt_m .  '.html#' . lc( $directive ) .  " - ";
     print $desc . "\n";
 
-    print "forget $name default\n";
+    print "forget $name default\n" if $opt_f;
     if ( $d->{default} ) {
         print "$name default is " . $d->{default} . "\n";
     } else {
         print "$name default is <reply>$name has no default value\n";
     }
 
-    print "forget $name override\n";
+    print "forget $name override\n" if $opt_f;
     if ( $d->{override} ) {
         print "$name override is <reply>$name may be used in an .htaccess file if AllowOverride is set to ".$d->{override}."\n";
     } else {
         print "$name override is <reply>$name may not be used in a .htaccess file\n";
     }
 
-    print "forget $name context\n";
+    print "forget $name context\n" if $opt_f;
     print "$name context is <reply>$name may be used in the following contexts: ";
     my $contexts = ref( $d->{contextlist}->{context} ) ?  $d->{contextlist}->{context} : [ $d->{contextlist}->{context} ];
     print ( join ', ', @{ $contexts } ) . "\n";
@@ -72,6 +72,8 @@ be fed to the IRC bot.
 -m mod_foo - Run for mod_foo.
 
 -v version - Generate URLs for this version. Defaults to "current"
+
+-f Adds the 'forget' statements to make the bot forget the existing version of the factoid.
 
 ~;
 
